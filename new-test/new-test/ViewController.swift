eb10c2c751ae37673cccd919d6244cc61097014e
Ikumi Sato <ikumin.gng@gmail.com>
2024-11-27 16:56:14 +0900

    Initial commit with project files and updates

---
+++ new-test/new-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  new-test
+//
+//  Created by 佐藤郁巳 on 2024/11/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Another test comment
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ new-test/new-testTests/new_testTests.swift
@@ -0,0 +1,17 @@
+//
+//  new_testTests.swift
+//  new-testTests
+//
+//  Created by 佐藤郁巳 on 2024/11/27.
+//
+
+import Testing
+@testable import new_test
+
+struct new_testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
